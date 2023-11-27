5478673f2b3fe8c1a583b7d18a58d524bb31ee89
sanchit0160 <36697768+sanchit0160@users.noreply.github.com>
2023-11-27 11:34:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const cors = require('cors');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 connectDB();
 
 // Init middleware
+app.use(cors()); // Allow all origins to access the server
 app.use(express.json({ extended: false }));
 
+// Set up your routes
 app.get('/', (req, res) => {
-  // Set the title in the HTML response
   res.send(`
     <html>
       <head>
         <link rel="icon" href="favicon.ico" />
-        <title>node.js Server</title>
+        <title>connect2pros Server</title>
       </head>
       <body>
         <h1>connect2pros server is running</h1>
@@ -30,5 +32,5 @@ app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 471;
 app.listen(PORT, () => {
-  return console.log(`Server started running on port ${PORT}`)
+  console.log(`Server started running on port ${PORT}`);
 });
